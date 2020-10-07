4885371868ad43ef0c8f2f70939fb3a61d9ce1f1
dominicdaviescodes <codewithdom@gmail.com>
2020-10-07 20:57:30 +0100

    add notes for lecture 10

--- basics/strings.js
+++ basics/strings.js
@@ -1,2 +1,16 @@
+// use separate variables and combine them
+
 let city = 'Philadelphia';
 let country = 'United States';
+// use the concatenation operator (+)
+let location = city + ', ' + country;
+console.log(location);
+
+// returns
+// $ node strings.js
+// Philadelphia, United States
+
+// EXERCISE
+
+// use variables city, country, location
+// use concatenation, return London, UK
